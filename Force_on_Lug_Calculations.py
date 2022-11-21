2b76434fa6d250b399bb8c03335f33720f4ab374
Frostydosty <ffjustin.yj.hu@gmail.com>
2022-11-21 14:58:23 +0100

    Update Force_on_Lug_Calculations.py

--- Force_on_Lug_Calculations.py
+++ Force_on_Lug_Calculations.py
@@ -1,25 +1,27 @@
 import numpy as np
 
 # Forces Location Lug
-Fx = 989 # N
-Fy = 989 # N
-Fz = -2250 # N
-Mx = 742 # Nm
-My = 1122 # Nm
-Mz = 5.87 # Nm
+Fx = 989    #N
+Fy = 989    #N
+Fz = -2250  #N
+Mx = 742    #Nm
+My = 1122   #Nm
+Mz = 5.87   #Nm
 
-hz = 1819/1000 # mm
-h = 16.25 # mm
-y = 1500 # mm
+hz = 1819/1000  #m
+h = 16.25/1000  #m
 
 F1 = Mx / hz
-P = np.sqrt( ((F1 + Fy) ** 2) + ((Fz) ** 2))
-theta = np.arctan(Fz / (F1 + Fy))
+F2 = My / hz
 
-Plug = P / 2
-Plug = round(Plug, 1)
-Fxlug = (Fx / 2) + My / hz
-Fxlug = round(Fxlug, 1)
+Sum_Fx = (Fx / 2) + F2  #maximum force in x direction on 1 of the lugs
+Sum_Fy = (Fy / 2) + F1
+Sum_Fz = (Fz / 2)
 
+P = np.sqrt(((Sum_Fy) ** 2) + ((Sum_Fz) ** 2))
+theta = np.arctan(abs(Sum_Fz / Sum_Fy)) * 180 / np.pi
+print(Sum_Fy, Sum_Fz)
 
-print("Aligned P force and x force on each lug respectively", Plug, Fxlug)
+print("The magnitude of the P force on each lug = %.1f [N], "
+      "and the angle theta CCW from the y-axis = %.1f [rad]." % (P, theta))
+print("The force on each lug in the x-direction = %.1f [N]." %Sum_Fx)
