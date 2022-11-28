efcf9bbb2d03f95c4b4c040bcc06d512bdcbb563
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2022-11-28 14:43:42 +0100

    Lug temperature calculations (completed)

--- lug_temp.py
+++ lug_temp.py
@@ -8,19 +8,31 @@ J_s_cold = 0            # [W/m^2] solar radiation
 J_a_cold = 0            # [W/m^2] albedo flux
 J_p_cold = 7348         # [W/m^2] planetary radiation
 
-# Dimensions
+# Geometry
 t_arm = 16.25 / 1000    # [m] arm thickness
-h = t_arm
-t_1 = ...
-D_2 = ...
-e_2 = ...
+h = t_arm               # [m] axle length
+t_1 = 10 / 1000
+D_2 = 5 / 1000
+e_2 = 20 / 1000
 R = t_1
-e = ...
+e = 40 / 1000
 W = 2 * e
-t_2 = ...
+t_2 = 10 / 1000
 h_1 = 2 * e
-alpha = 1
-epsilon = 1
+
+# Material properties
+alpha_hot = 0.40
+alpha_cold = 0.10
+epsilon_hot = 0.07
+epsilon_cold = 0.77
+k_lug = 130
+
+# Spacecraft properties
+k_sc = 170
+T_sc_hot = 357
+T_sc_cold = 310
+h_c = 1.5 * 10 ** 3
+t_skin = 4 / 1000
 
 # Constants
 sigma =  5.670374419 * 10 ** -8 # [W/m^2 K^4]
@@ -31,16 +43,38 @@ bend_area = (1 - np.pi / 4) * R ** 2
 side_area = W * t_2 + e * W + 1/2 * np.pi * e ** 2
 top_area = L * W - 4*np.pi*D_2**2
 
-front_area = L * t_2 + 2 * t_1 * h_1 + 2 * bend_area
-emission_area = 2 * front_area + 2 * side_area + top_area
+side_area = L * t_2 + 2 * t_1 * h_1 + 2 * bend_area
+sun_area = max(side_area, top_area)
+emission_area = 2 * sun_area + 2 * side_area + top_area
 conduction_area = top_area
 
-# Hot case
-Q_absorbed_hot = front_area * alpha * (J_s_hot + J_a_hot + J_p_hot)
-T_hot = (Q_absorbed_hot / (emission_area * sigma * epsilon)) ** (1 / 4)
-print(f'{T_hot = }')
+denom = t_2 / (conduction_area * k_lug) + 1/(h_c * conduction_area) + t_skin/(k_sc * conduction_area)
 
-# Cold case
-Q_absorbed_cold = front_area * alpha * (J_s_cold + J_a_cold + J_p_cold)
-T_cold = (Q_absorbed_cold / (emission_area * sigma * epsilon)) ** (1 / 4)
-print(f'{T_cold = }')
+hot_coefficients = [
+    -sun_area * alpha_hot * (J_s_hot + J_a_hot + J_p_hot) - T_sc_hot / denom,
+    1/denom,
+    0,
+    0,
+    emission_area * sigma * epsilon_hot
+]
+
+hot_equation = np.polynomial.polynomial.Polynomial(hot_coefficients)
+hot_roots = hot_equation.roots()
+
+cold_coefficients = [
+    -sun_area * alpha_cold * (J_s_cold + J_a_cold + J_p_cold) - T_sc_cold / denom,
+    1/denom,
+    0,
+    0,
+    emission_area * sigma * epsilon_cold
+]
+
+cold_equation = np.polynomial.polynomial.Polynomial(cold_coefficients)
+cold_roots = cold_equation.roots()
+
+for z in hot_roots:
+    if z.imag == 0 and z.real > 0:
+        print(f'Hot case: {z.real} K')
+for z in cold_roots:
+    if z.imag == 0 and z.real > 0:
+        print(f'Cold case: {z.real} K')
